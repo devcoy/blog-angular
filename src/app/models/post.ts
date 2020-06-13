42869f7df4ff4cdcee3fd937d304a05eae3cf10f
Jorge Cervantes <jorgecdev@gmail.com>
2020-06-13 15:39:18 -0500

    se crearon los modelos de User, Category y Post

---
+++ src/app/models/post.ts
@@ -0,0 +1,12 @@
+export class Post {
+  constructor(
+    id: number,
+    userId: number,
+    categoryId: number,
+    title: string,
+    content: string,
+    image: string,
+    createdAt: any,
+    updatedAt: any
+  ) { }
+}
---
+++ src/app/models/user.ts
@@ -0,0 +1,15 @@
+export class User {
+  constructor(
+    id: number,
+    name: string,
+    surname: string,
+    role: string,
+    email: string,
+    password: string,
+    description: string,
+    image: string,
+    createdAt: any,
+    updatedAt: any
+
+  ) {}
+}
