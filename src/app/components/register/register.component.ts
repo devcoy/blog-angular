3aecbd9f0a3a0707d889e6ee9f1d66ad5c1af761
Jorge Cervantes <jorgecdev@gmail.com>
2020-06-13 18:44:03 -0500

    maquetacion del formulario de registro

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
+
 
 @Component({
   selector: 'app-register',
@@ -7,7 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  pageTitle: string;
+  user: User;
+
+  constructor() { 
+    this.pageTitle = 'Registro';
+    this.user = new User(null, '', '', 'ROLE_USER', '', '', '', '', null, null);
+  }
 
   ngOnInit() {
   }
