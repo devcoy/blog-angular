7c1725604312c405ee6d28ec3275e443465c3daa
Jorge Cervantes <jorgecdev@gmail.com>
2020-06-14 02:49:30 -0500

    cerrar sesion

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/models/user';
 import { UserService } from 'src/app/services/user.service';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 
 @Component({
@@ -18,21 +19,21 @@ export class LoginComponent implements OnInit {
   identity: any;
 
   constructor(
-    private _userService: UserService
+    private _userService: UserService,
+    private _router: Router, // Redireccionar
+    private _route: ActivatedRoute // Obtner parámetros de la url
   ) {
     this.pageTitle = 'Iniciar sesión';
     this.user = new User(null, '', '', 'ROLE_USER', '', '', '', '', null, null);
   }
 
   ngOnInit() {
+    // Se ejecuta siempre y cierra sesión solo cuando le llegua el parámetro :sure en la url
+    this.logout();
   }
 
   /**
    * Iniciar sesión
-   * @param user Objeto de usuario
-   * @param getToken true: obtendra los datos del usuario identificado
-   * 
-   * @return token o data_user
    */
   onSubmit(form) {
     //console.log(this.user);
@@ -52,6 +53,9 @@ export class LoginComponent implements OnInit {
               localStorage.setItem('token', this.token);
               // Convertir el obj de js a json string
               localStorage.setItem('identity', JSON.stringify(this.identity));
+
+              // Redirección
+              this._router.navigate(['inicio']);
             },
             error => {
               this.status = response.status;
@@ -70,6 +74,28 @@ export class LoginComponent implements OnInit {
         this.message = error.error.message;
       }
     );
+  }
+
+  /**
+   * Cerrar sesión
+   */
+  logout() {
+    // Obtener el parámetro de la url
+    this._route.params.subscribe(params => {
+      let logout = +params['sure']; // Obtenernos el parámetro :sure
+
+      // Remuevo lo que esta almacenado en localStorage y elimino las valores de las propiedades
+      if (logout === 1) {
+        localStorage.removeItem('token');
+        localStorage.removeItem('identity');
+
+        this.token = null;
+        this.identity = null;
+
+        // Redirección
+        this._router.navigate(['inicio']);
+      }
+    });
   }
 
 }
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, DoCheck } from '@angular/core';
 import { UserService } from 'src/app/services/user.service';
 import { User } from 'src/app/models/user';
 import { Global } from 'src/app/services/global.service';
@@ -11,19 +11,28 @@ import { identity } from 'rxjs';
   styleUrls: ['./navbar.component.scss'],
   providers: [UserService]
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, DoCheck {
 
   token: string;
   identity: any;
 
   constructor(
     private _userService : UserService
-  ) { 
-    this.token = this._userService.getToken();
-    this.identity = this._userService.getIdentity();    
-  }
+  ) { }
 
   ngOnInit() {
+    // Ejecuto para actualizar el navbar
+    this.loadUser();
+  }
+
+  ngDoCheck () {
+    // Ejecuto para actualizar el navbar
+    this.loadUser();
+  }
+
+  loadUser() {
+    this.token = this._userService.getToken();
+    this.identity = this._userService.getIdentity();    
   }
 
 }
